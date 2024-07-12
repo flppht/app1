40eddd269ec7251d77c897693b5b5bfaa3dceffe
filip <filip.kulasinac@singularity.is>
2024-07-12 13:57:12 +0200

    added widget

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="">
+        This is some app that is using embedded widget
+      </div>
+    </div>
+  );
+}
+
+export default App;
